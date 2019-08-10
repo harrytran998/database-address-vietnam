6390d3c0b56711830d5abcc433a2514e0fd4d7bf
NhatDeepTrai <demonslight998@gmail.com>
2019-08-10 13:55:29 +0700

    :sparkles: feat: Add constant status https codes

---
+++ constant/status.js
@@ -0,0 +1,11 @@
+/** HTTP Status code */
+export const STATUS_OK = 200
+export const STATUS_CREATED = 201
+export const STATUS_ACCEPTED = 202
+export const STATUS_BAD_REQUEST = 400
+export const STATUS_UNAUTHORIZED = 401
+export const STATUS_FORBIDDEN = 403
+export const STATUS_NOT_FOUND = 404
+export const STATUS_NOT_ACCEPTABLE = 406
+export const STATUS_UNPROCESSABLE_ENTITY = 422
+export const STATUS_INTERNAL_SERVER_ERROR = 500
